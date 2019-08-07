823a5c1742f22b0c8fe671b07de3142530c1571d
Elliot Kozil <edk128@gmail.com>
2019-08-06 22:43:16 -0500

    Improve server response, log

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const port = process.env.PORT || 4000
 
 app.get('/', function(req, res) {
     console.log('Get /')
+    res.send('Got /')
 });
 
 app.listen(port, function(){
-    console.log(`Nodejs listening on port ${port}`);
+    console.log(`Express listening on port ${port}`);
     console.log(`Nodejs version ${process.version}`)
 });
