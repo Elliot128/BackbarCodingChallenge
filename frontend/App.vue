4b1376b70486a87bfbd28dd267a7976939836379
Elliot Kozil <edk128@gmail.com>
2019-08-08 21:40:13 -0500

    Add Vue App component

---
+++ frontend/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello World'
+    }
+  },
+  created () {
+      setTimeout(() => {
+          this.msg = 'Goodbye World';
+      }, 5000)
+  }
+}
+</script>
+
+<style lang="css">
+  #app {
+    color: #5e03d4;
+  }
+</style>
