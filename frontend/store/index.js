2710f62f7393721ecd9214f30482297eea7a299a
Elliot Kozil <edk128@gmail.com>
2019-08-17 10:26:50 -0500

    Implement msgs, code actions/mutation in vuex

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex';
 import Constants from '../constants';
 import Axios from 'axios';
 
-const { MUTATION_TYPES } = Constants;
+const { ACTION_TYPES, MUTATION_TYPES } = Constants;
 
 Vue.use(Vuex);
 
@@ -22,11 +22,32 @@ const Api = Axios.create({
 
 export default new Vuex.Store({
     state: {
-        msg: 'Hello World'
+        code: '',
+        msgs: [],
+    },
+    actions: {
+        async [ACTION_TYPES.GET_CODE] ({ commit }) {
+            const code = await Api.get('/code');
+            commit(MUTATION_TYPES.SET_CODE, code);
+        },
+        async [ACTION_TYPES.GET_MSGS] ({ commit }) {
+            const msgs = await Api.get('/messages');
+            commit(MUTATION_TYPES.SET_MSGS, msgs);
+        },
+        async [ACTION_TYPES.ADD_MSG] ({ commit }, payload) {
+            await Api.post(`/messages/${payload}`);
+            commit(MUTATION_TYPES.ADD_MSG, payload);
+        },
     },
     mutations: {
-        [MUTATION_TYPES.SET_MSG] (state, payload) {
-            state.msg = payload;
-        }
+        [MUTATION_TYPES.SET_CODE] (state, payload) {
+            state.code = payload;
+        },
+        [MUTATION_TYPES.SET_MSGS] (state, payload) {
+            state.msgs = payload;
+        },
+        [MUTATION_TYPES.ADD_MSG] (state, payload) {
+            state.msgs.push(payload);
+        },
     }
 });
