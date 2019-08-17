dae42b4bf898104cb1e5e39b721601ab65bc82a4
Elliot Kozil <edk128@gmail.com>
2019-08-17 09:37:02 -0500

    create and store in memory sessions

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const session = require('express-session');
+const crypto = require('crypto');
 
 app.use('/dist',
     express.static(path.join(__dirname, 'dist'))
 );
 
+app.use(session({
+    secret: 'session secret',
+    saveUninitialized: true,
+    resave: true,
+    cookie: {
+        secure: true
+    },
+}));
+
 app.get('/', (req, res) => {
+    req.session.code = req.query.code 
+        ? req.query.code 
+        : (crypto.randomBytes(16)).toString('hex');
     res.sendFile(
         path.join(__dirname, 'dist/index.html')
     );
